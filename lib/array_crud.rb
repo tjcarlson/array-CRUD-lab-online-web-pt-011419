e525e8119fae3304c07cd7b7a0bde9dc9dcca2dd
Tim Carlson <writetimcarlson@gmail.com>
2019-01-20 23:39:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,6 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
       array = ["wow", "I", "am", "really", "learning", "arrays!"]
+      array[0]
 end
 
 def retrieve_last_element_from_array(array)
